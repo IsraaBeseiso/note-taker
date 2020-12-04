f670de8f92c37954c52d01fbe50a3e82d81e565f
Israa Beseiso <israabeseiso@gmail.com>
2020-12-04 08:47:03 -0700

    added changes

--- routes.js
+++ routes.js
@@ -1,31 +1,52 @@
 const fs = require('fs');
 const path = require('path');
+const util = require('util')
+const readFileAsync= util.promisify(fs.readFile)
 
 module.exports = app => {
-
+function getNotes(note){
+    let newNote = note
+    readFileAsync('db/db.json', 'utf8', function(err, data){
+        if(err) throw err
+        return data
+    }).then(
+        notes => [...notes, newNote]
+    ).then( updatedNotes => fs.writeFileSync('db/db.json', JSON.stringify(updatedNotes)))
+    .then(()=> newNote)
+}
     // Setup notes variable
-    fs.readFile("db/db.json","utf8", (err, data) => {
+    // fs.readFile("db/db.json","utf8", (err, data) => {
 
-        if (err) throw err;
+    //     if (err) throw err;
+    
 
-        var notes = JSON.parse(data);
-
+    //     var notes = JSON.parse(data);
+    // })
         // API ROUTES
         // ========================================================
     
         // Setup the /api/notes get route
         app.get("/api/notes", function(req, res) {
-            // Read the db.json file and return all saved notes as JSON.
-            res.json(notes);
+            var note = []
+            fs.readFile("db/db.json","utf8", (err, data) => {
+
+                if (err) throw err;
+            
+        
+                 note = note.concat(JSON.parse(data));
+            })
+            res.json(note)
         });
 
         // Setup the /api/notes post route
         app.post("/api/notes", function(req, res) {
             // Receives a new note, adds it to db.json, then returns the new note
-            let newNote = req.body;
-            notes.push(newNote);
-            updateDb();
-            return console.log("Added new note: "+newNote.title);
+           getNotes(req.body)
+           .then((note) => res.json(note))
+           .catch((err) => res.status(500).json(err))
+            // notes.push(newNote);
+            // fs.writeFileSync('db/db.json', JSON.stringify(notes))
+            // return console.log("Added new note: " + newNote.title);
         });
 
         // Retrieves a note with specific id
@@ -46,12 +67,12 @@ module.exports = app => {
 
         // Display notes.html when /notes is accessed
         app.get('/notes', function(req,res) {
-            res.sendFile(path.join(__dirname, "../public/notes.html"));
+            res.sendFile(path.join(__dirname, "./public/notes.html"));
         });
         
         // Display index.html when all other routes are accessed
         app.get('*', function(req,res) {
-            res.sendFile(path.join(__dirname, "../public/index.html"));
+            res.sendFile(path.join(__dirname, "./public/index.html"));
         });
 
         //updates the json file whenever a note is added or deleted
@@ -62,6 +83,6 @@ module.exports = app => {
             });
         }
 
-    });
+   
 
 }
