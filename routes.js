945714787c8efe1d5ffd5066cb938cfed418e672
Israa Beseiso <israabeseiso@gmail.com>
2021-01-04 10:38:00 -0700

    updated post route

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
-const path = require('path');
-const util = require('util')
-const readFileAsync= util.promisify(fs.readFile)
+const {v4: uuidv4} = require("uuid")
+
 
 module.exports = app => {
 app.get('/api/notes', function(req, res){
@@ -15,30 +14,29 @@ app.get('/api/notes', function(req, res){
         res.send(notes)
     })
 })
-   
+   app.post("/api/notes", function(req, res){
+       var currentNotes = []
+       const {title, text } = req.body
+       const newNote = {
+           title, text, id: uuidv4
+       }
 
-        // Setup the /api/notes post route
-        app.post("/api/notes", function(req, res) {
-            // Receives a new note, adds it to db.json, then returns the new note
-            let newNote = req.body
-    writeFileAsync('db/db.json', 'utf8', function(err, data){
-        if(err) throw err
-        return data
-    }).then(
-        notes => [...notes, newNote]
-    ).then( updatedNotes => fs.writeFileSync('db/db.json', JSON.stringify(updatedNotes)))
-    .then(()=> newNote)
-           .catch((err) => res.status(500).json(err))
-            // notes.push(newNote);
-            // fs.writeFileSync('db/db.json', JSON.stringify(notes))
-            // return console.log("Added new note: " + newNote.title);
-        });
+       fs.readFile('db/db.json', function(err, data){
+           if (err){
+               console.log(err)
+           }
 
-        // Retrieves a note with specific id
-        app.get("/api/notes/:id", function(req,res) {
-            // display json for the notes array indices of the provided id
-            res.json(notes[req.params.id]);
-        });
+           currentNotes = JSON.parse(data) || [];
+           currentNotes.push(newNote);
+
+           fs.writeFile('db/db.json', JSON.stringify(currentNotes), function(err){
+               if(err){
+                   console.log(err)
+               }
+           })
+
+       })
+   })
 
         // Deletes a note with specific id
         app.delete("/api/notes/:id", function(req, res) {
@@ -46,15 +44,5 @@ app.get('/api/notes', function(req, res){
             updateDb();
             console.log("Deleted note with id "+req.params.id);
         });
-
-        //updates the json file whenever a note is added or deleted
-        function updateDb() {
-            fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
-                if (err) throw err;
-                return true;
-            });
-        }
-
-   
 
 }
